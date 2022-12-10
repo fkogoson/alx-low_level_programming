a751c1dcd9e3b97ac6efc6804e39546b252d4e4e
Festus Kogo <fkogoson@gmail.com>
2022-12-09 23:49:50 -0800

    Updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
 /**
- * C program that prints exactly \"Programming is like building
- *
- *a multilingual puzzle, followed by a new line.
- *
- * Use the function puts
- *
- * You are not allowed to use printf
- *
- * Your program should end with the value 0
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
