57e8b4115215f774369ac0d7c42f43ab98c56d7a
Festus Kogo <fkogoson@gmail.com>
2022-12-13 05:13:20 -0800

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	/* your code goes here */
+	int i;
+
+	for (i = 0; i < 10;i++)
+	{
+		printf("%d",i);
+	}
+	putchar('\n');
+
+	return (0);
+}
