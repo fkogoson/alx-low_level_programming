1818f35c72b8b0bb9c6ac6bbef5b2a04c88d8a26
Festus Kogo <fkogoson@gmail.com>
2022-12-14 11:52:39 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet, in lowercase, followed by a new line
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+
+}
