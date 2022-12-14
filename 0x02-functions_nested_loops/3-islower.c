10c1ebc92e60f5a560bbecee9bc5f579bf1272c0
Festus Kogo <fkogoson@gmail.com>
2022-12-14 12:17:13 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main -  checks for lowercase character
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	int r;
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1) 
+	}
+	else
+	{
+		return (0);
+	}
+}
